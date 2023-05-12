fb91b30f8e4fa324ffa27a862f086200faa3be95
chetannn <chetankharel7@gmail.com>
2023-05-12 19:57:26 +0545

    removed search highlight and key bindings to move lines

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -3,3 +3,5 @@ vim.g.maplocalleader = ' '
 
 vim.keymap.set("x","<leader>p", "\"_dp")
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") --  https://youtu.be/w7i4amO_zaE?t=1545 (Thanks prime)
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
