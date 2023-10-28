a3498b7eec94cc5f1ad9cc689bff8b6f5c761491
chetannn <chetankharel7@gmail.com>
2023-10-28 16:55:05 +0545

    wip

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,58 +1,57 @@
-local lsp_zero = require('lsp-zero')
+local lsp_zero = require("lsp-zero")
 local null_ls = require("null-ls")
-local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
+local cmp = require("cmp")
+local cmp_action = require("lsp-zero").cmp_action()
 
 lsp_zero.on_attach(function(client, bufnr)
-  lsp_zero.default_keymaps({buffer = bufnr})
-  lsp_zero.buffer_autoformat()
+	lsp_zero.default_keymaps({ buffer = bufnr })
+	lsp_zero.buffer_autoformat()
 end)
 
-lsp_zero.setup_servers({'tsserver', 'rust_analyzer', 'tailwindcss'})
+lsp_zero.setup_servers({ "tsserver", "rust_analyzer", "tailwindcss" })
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  ensure_installed = {
-  'tsserver',
-  'rust_analyzer',
-  'gopls',
-  'tailwindcss'
-  },
-  handlers = {
-    lsp_zero.default_setup,
-    lua_ls = function()
-      local lua_opts = lsp_zero.nvim_lua_ls()
-      require('lspconfig').lua_ls.setup(lua_opts)
-    end,
-  }
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	ensure_installed = {
+		"tsserver",
+		"rust_analyzer",
+		"gopls",
+		"tailwindcss",
+	},
+	handlers = {
+		lsp_zero.default_setup,
+		lua_ls = function()
+			local lua_opts = lsp_zero.nvim_lua_ls()
+			require("lspconfig").lua_ls.setup(lua_opts)
+		end,
+	},
 })
 
 lsp_zero.format_on_save({
-  format_opts = {
-    async = false,
-    timeout_ms = 10000,
-  },
-  servers = {
-    ['null-ls'] = {'javascript', 'typescript', 'typescriptreact', 'lua', 'go' },
-    ['rust_analyzer'] = {'rust'},
-    ['gofumpt'] = {'go'},
-  }
+	format_opts = {
+		async = false,
+		timeout_ms = 10000,
+	},
+	servers = {
+		["null-ls"] = { "javascript", "typescript", "typescriptreact", "lua", "go" },
+		["rust_analyzer"] = { "rust" },
+	},
 })
 
 cmp.setup({
-  mapping = {
-    ['<Tab>'] = cmp_action.tab_complete(),
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
-  }
+	mapping = {
+		["<Tab>"] = cmp_action.tab_complete(),
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
+	},
 })
 
 local null_ls = require("null-ls")
 
 null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.rustywind,
-        null_ls.builtins.completion.spell,
-        null_ls.builtins.formatting.gofumpt
-    },
+	sources = {
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.completion.spell,
+		null_ls.builtins.formatting.gofumpt,
+		null_ls.builtins.formatting.stylua,
+	},
 })
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,39 +1,53 @@
 return {
-    { 'rebelot/kanagawa.nvim' },
-    { 'tpope/vim-commentary' },
-    { "tpope/vim-surround" },
-    { "tpope/vim-sleuth" },
-    { "christoomey/vim-tmux-navigator" },
-    { "tpope/vim-sleuth" },
-    { "nvim-treesitter/nvim-treesitter",
-        build = function()
-            require('nvim-treesitter.install').update({ with_sync = true })
-        end
-    },
-    { "VonHeikemen/lsp-zero.nvim", branch = 'v3.x', },
-  -- LSP Support
-   {'neovim/nvim-lspconfig'},
-   {'williamboman/mason.nvim'},
-   {'williamboman/mason-lspconfig.nvim'},
-  -- Autocompletion
-   {'hrsh7th/nvim-cmp'},
-   {'hrsh7th/cmp-nvim-lsp'},
-   {'L3MON4D3/LuaSnip'},
-   { "jose-elias-alvarez/null-ls.nvim", dependencies = { 'nvim-lua/plenary.nvim' } },
-   {
-    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
-   },
-   { "catppuccin/nvim", name = "catppuccin" },
-   { "nvim-lualine/lualine.nvim",
-      requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-    },
-   { 
-    'olivercederborg/poimandres.nvim',
-     priority = 1000,
-    },
-  {
-  'Exafunction/codeium.vim',
-  event = 'BufEnter'
- }
+	{ "rebelot/kanagawa.nvim" },
+	{ "tpope/vim-commentary" },
+	{ "tpope/vim-surround" },
+	{ "tpope/vim-sleuth" },
+	{ "christoomey/vim-tmux-navigator" },
+	{ "tpope/vim-sleuth" },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = function()
+			require("nvim-treesitter.install").update({ with_sync = true })
+		end,
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+			configs.setup({
+				ensure_installed = { "javascript", "typescript", "rust", "c", "lua", "query", "go" },
+				sync_install = false,
+				auto_install = true,
+				indent = {
+					enable = true,
+				},
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
+			})
+		end,
+	},
+	{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
+	-- LSP Support
+	{ "neovim/nvim-lspconfig" },
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
+	-- Autocompletion
+	{ "hrsh7th/nvim-cmp" },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "L3MON4D3/LuaSnip" },
+	{ "jose-elias-alvarez/null-ls.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+	{
+		"nvim-telescope/telescope-file-browser.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+	},
+	{ "catppuccin/nvim", name = "catppuccin" },
+	{ "nvim-lualine/lualine.nvim", requires = { "nvim-tree/nvim-web-devicons", opt = true } },
+	{
+		"olivercederborg/poimandres.nvim",
+		priority = 1000,
+	},
+	{
+		"Exafunction/codeium.vim",
+		event = "BufEnter",
+	},
 }
