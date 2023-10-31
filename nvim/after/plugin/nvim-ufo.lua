f29234b045e048fb8a1c2ff49c1fc046ec011d4e
chetannn <chetankharel7@gmail.com>
2023-10-31 08:56:21 +0545

    wip

--- nvim/after/plugin/nvim-ufo.lua
+++ nvim/after/plugin/nvim-ufo.lua
@@ -2,6 +2,7 @@ vim.o.foldcolumn = "1" -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 
 vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
@@ -53,21 +54,6 @@ for _, ls in ipairs(language_servers) do
 end
 
 require("ufo").setup({
-	open_fold_hl_timeout = 150,
-	close_fold_kinds = { "imports", "comment" },
-	preview = {
-		win_config = {
-			border = { "", "─", "", "", "", "─", "", "" },
-			winhighlight = "Normal:Folded",
-			winblend = 0,
-		},
-		mappings = {
-			scrollU = "<C-u>",
-			scrollD = "<C-d>",
-			jumpTop = "[",
-			jumpBot = "]",
-		},
-	},
 	fold_virt_text_handler = handler,
 	provider_selector = function(bufnr, filetype, buftype)
 		return ftMap[filetype]
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -50,5 +50,24 @@ return {
 		"Exafunction/codeium.vim",
 		event = "BufEnter",
 	},
-	{ "kevinhwang91/nvim-ufo", dependencies = "kevinhwang91/promise-async" },
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = {
+			"kevinhwang91/promise-async",
+			{
+				"luukvbaal/statuscol.nvim",
+				config = function()
+					local builtin = require("statuscol.builtin")
+					require("statuscol").setup({
+						relculright = true,
+						segments = {
+							{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+							{ text = { "%s" }, click = "v:lua.ScSa" },
+							{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+						},
+					})
+				end,
+			},
+		},
+	},
 }
