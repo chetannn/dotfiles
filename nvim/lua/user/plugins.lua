b779880abcea7117ef6768d1989a1c7c0f05df2b
chetannn <chetankharel7@gmail.com>
2024-01-06 21:37:44 +0545

    added rose-pine theme

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -103,4 +103,21 @@ return {
 			})
 		end,
 	},
+	{
+		{
+			"rose-pine/neovim",
+			name = "rose-pine",
+			config = function()
+				require("rose-pine").setup({
+					variant = "moon",
+					dark_variant = "moon",
+					bold_vert_split = false,
+					dim_nc_background = false,
+					disable_background = false,
+					disable_float_background = false,
+					disable_italics = true,
+				})
+			end,
+		},
+	},
 }
