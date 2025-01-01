5ae6bffb02a4c1cc40230df1c23a2c49a7fd67a5
chetannn <chetankharel7@gmail.com>
2025-01-01 21:52:14 +0545

    feat: add ghosty keybinds and lsp completion

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -5,20 +5,26 @@ return {
 		dependencies = {
 			"williamboman/mason.nvim",
 			"williamboman/mason-lspconfig.nvim",
+			"hrsh7th/cmp-nvim-lsp",
 		},
 		config = function()
 			local mason = require("mason")
 			local mason_lspconfig = require("mason-lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			mason.setup({})
 
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 			mason_lspconfig.setup({
-				ensure_installed = { "tsserver", "jsonls", "rust_analyzer" },
+				ensure_installed = { "tsserver", "jsonls", "rust_analyzer", "tailwindcss", "svelte" },
 			})
 
 			mason_lspconfig.setup_handlers({
 				function(server_name) -- default handler (optional)
-					require("lspconfig")[server_name].setup({})
+					require("lspconfig")[server_name].setup({
+						capabilities,
+					})
 				end,
 			})
 		end,
--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -5,6 +5,20 @@ return {
 		config = function()
 			require("oil").setup({
 				skip_confirm_for_simple_edits = true,
+				view_options = {
+					show_hidden = false,
+					is_hidden_file = function(name, bufnr)
+						return name == ".git"
+					end,
+					is_always_hidden = function(name, bufnr)
+						return false
+					end,
+					natural_order = true,
+					sort = {
+						{ "type", "asc" },
+						{ "name", "asc" },
+					},
+				},
 			})
 		end,
 	},
